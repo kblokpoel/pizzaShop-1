e350d1215682840279f06cf7414938e59daf42d4
Johnty <302052166@student.rocmondriaan.nl>
2022-03-30 09:11:35 +0200

    week 3 af
    hhhh

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -3,9 +3,12 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 
 class PizzaController extends AbstractController
@@ -13,8 +16,29 @@ class PizzaController extends AbstractController
     /**
      * @Route("/")
      */
+
+    public function homepage(CategoryRepository $em) {
+//        $categories = [
+//            "vis",
+//            "vega",
+//            "vlees"
+//        ];
+        /** @var Category $cat */
+        $categories = $em->findAll();
+
+        return $this->render('pizza/home.html.twig', [
+            'categories' => $categories
+        ]);
+
+    }
+
+    /**
+     * @Route("/aa")
+     */
+
     public function randomPizza(): Response
     {
+
         $pizzas = [
             "PIZZA PEPPERONI DELUXE",
             "PIZZA PEPPERONI PARTY",
@@ -25,16 +49,17 @@ class PizzaController extends AbstractController
             "PIZZA HAM",
             "PIZZA FUNGHI"
         ];
-
+        global $categories;
 
 //        return new Response(
 //            '<html><body>Lucky pizza:' . $pizzas[array_rand($pizzas)] . '</body></html>'
 //        );
         return $this->render('pizza/home.html.twig', [
-            'pizzas' => $pizzas
+            'categories' => $categories
         ]);
     }
     //categories page
+
     /**
      * @Route("/categories/{slug}")
      */
@@ -44,4 +69,17 @@ class PizzaController extends AbstractController
         ));
 
     }
+
+    /**
+     * @Route("/contact")
+     */
+
+    public function contact(): Response {
+
+        return $this->render('pizza/contact.html.twig', [
+
+        ]);
+    }
+
+
 }

1a19df6192e5896a2d5140f229a5333b73ee172c
kblokpoel <70716066+kblokpoel@users.noreply.github.com>
2022-05-20 14:28:25 +0200

    Add files via upload

---
+++ pizza.sql
@@ -0,0 +1,258 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 20 mei 2022 om 14:26
+-- Serverversie: 10.4.22-MariaDB
+-- PHP-versie: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pizza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `image`, `description`) VALUES
+(1, 'Vlees', '/img/vleesPizza.jpg', 'Lekkerste vlees pizza\'s'),
+(2, 'Vegan', '/img/vegaPizza.png', 'Vegan pizza\'s'),
+(3, 'Kaas ', '/img/4Cheese.jpg', 'Kaas pizza\'s');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20220520113013', '2022-05-20 13:30:18', 64),
+('DoctrineMigrations\\Version20220520113116', '2022-05-20 13:31:20', 65),
+('DoctrineMigrations\\Version20220520113739', '2022-05-20 13:37:43', 32),
+('DoctrineMigrations\\Version20220520114647', '2022-05-20 13:46:52', 53),
+('DoctrineMigrations\\Version20220520114833', '2022-05-20 13:48:43', 31),
+('DoctrineMigrations\\Version20220520114919', '2022-05-20 13:49:21', 56);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `zipcode` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pizza_id` int(11) DEFAULT NULL,
+  `size_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `order`
+--
+
+INSERT INTO `order` (`id`, `fname`, `lname`, `address`, `city`, `zipcode`, `status`, `pizza_id`, `size_id`) VALUES
+(3, 'Keren', 'Blokpoel', 'Cartesiusstraat 214', 'Den Haag', '2562SR', 'pending', 1, 2),
+(4, 'fjfj', 'jgjhgjhg', 'jhgjhgh', 'gjhgjgj', 'gjj', 'pending', 1, 1),
+(5, 'unun', 'nj', 'unuu8', 'ju8ju8ju8', 'ju8ju8ju8', 'pending', 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `pizza`
+--
+
+CREATE TABLE `pizza` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `pizza`
+--
+
+INSERT INTO `pizza` (`id`, `name`, `image`, `description`, `price`, `category_id`) VALUES
+(1, 'BBQ MONSTER PIZZA', '/img/BBQpizza.jpg', 'BBQ PIZZA VOOR SERIEUS ETEN ', '12.99', 1),
+(2, 'Broccoli pizza', '/img/broccoliPizza.jpg', 'Waarom zou je dit kopen', '9.99', 2),
+(3, 'Pizza 4 Cheese', '/img/4Cheese.jpg', 'Pizza 4 cheese', '6.99', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `size`
+--
+
+CREATE TABLE `size` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `size`
+--
+
+INSERT INTO `size` (`id`, `name`) VALUES
+(1, 'S'),
+(2, 'M'),
+(3, 'L'),
+(4, 'calzone');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indexen voor tabel `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Indexen voor tabel `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_F5299398D41D1D42` (`pizza_id`),
+  ADD KEY `IDX_F5299398498DA827` (`size_id`);
+
+--
+-- Indexen voor tabel `pizza`
+--
+ALTER TABLE `pizza`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_CFDD826F12469DE2` (`category_id`);
+
+--
+-- Indexen voor tabel `size`
+--
+ALTER TABLE `size`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT voor een tabel `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT voor een tabel `pizza`
+--
+ALTER TABLE `pizza`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT voor een tabel `size`
+--
+ALTER TABLE `size`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `FK_F5299398498DA827` FOREIGN KEY (`size_id`) REFERENCES `size` (`id`),
+  ADD CONSTRAINT `FK_F5299398D41D1D42` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`id`);
+
+--
+-- Beperkingen voor tabel `pizza`
+--
+ALTER TABLE `pizza`
+  ADD CONSTRAINT `FK_CFDD826F12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
